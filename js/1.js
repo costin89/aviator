46dcc104e7828a665cbc2f23d3a4e0554de604f8
costin89 <mailtoast@googlemail.com>
2022-10-19 23:00:47 +0200

    Update 1.js

--- js/1.js
+++ js/1.js
@@ -32,7 +32,7 @@ mtlLoader.load("3d/WALLET.mtl", function(materials){
 });*/
 
 const gltfLoader = new THREE.GLTFLoader();
-const url = '3d/1.glb';
+const url = '3d/wallet.glb';
 gltfLoader.load(url, (gltf) => {
 	const root = gltf.scene;
     	scene.add(root);
